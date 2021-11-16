f0710b1c05baa43a6301129d90ba71166e7a009b
Janvis4 <94413720+Janvis4@users.noreply.github.com>
2021-11-16 11:33:44 +1100

    Update index.php

--- index.php
+++ index.php
@@ -36,19 +36,18 @@ switch( $action ) {
             $task_list = array_values($task_list);
         }
         break;
-    case 'Modify item':
-          $task_index = filter_input(INPUT_POST, 'taskid', FILTER_VALIDATE_INT);
-        if ($task_index === NULL || $task_index === FALSE) {
-            $errors[] = 'The items cannot be modified.';
-        } else {
-            unset($task_list[$task_index]);
-            $task_list = array_values($task_list);
-        }
-        break;
+    
 /*
     case 'Modify Task':
-    
+        $task_index = filter_input(INPUT_POST, 'taskid', FILTER_VALIDATE_INT);
+        if ($task_index === NULL || $task_index === FALSE) {
+           $errors[] = 'The task cannot be modified.';
+        } else {
+        $task_to_modify = $task_list[$task_index];
+        }    
+        break;
     case 'Save Changes':
+    
     
     case 'Cancel Changes':
     
