8a5d9718ec0acab906dea5696d032bda3b4defa4
Janvis4 <94413720+Janvis4@users.noreply.github.com>
2021-11-21 16:11:29 +1100

    Update delete_product.php

--- delete_product.php
+++ delete_product.php
@@ -3,7 +3,7 @@ require_once('database.php');
 
 // Get IDs
 $product_id = filter_input(INPUT_POST, 'product_id', FILTER_VALIDATE_INT);
-//$category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
+
 
 // Delete the product from the database
 if ($product_id != false) {
