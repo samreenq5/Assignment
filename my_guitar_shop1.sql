a5491ac2dafab3e4fa5d78b0f8f78da080b6094f
samreenq5 <94154003+samreenq5@users.noreply.github.com>
2021-11-21 16:09:38 +1100

    Update my_guitar_shop1.sql

--- my_guitar_shop1.sql
+++ my_guitar_shop1.sql
@@ -4,31 +4,11 @@ CREATE DATABASE my_guitar_shop1;
 USE my_guitar_shop1;  -- MySQL command
 
 -- create the tables
-/*CREATE TABLE categories (
-  categoryID       INT(11)        NOT NULL   AUTO_INCREMENT,
-  categoryName     VARCHAR(255)   NOT NULL,
-  PRIMARY KEY (categoryID)
-); */
-
 CREATE TABLE products (
   productID        INT(11)        NOT NULL   AUTO_INCREMENT,
---  categoryID       INT(11)        NOT NULL,
   productName      VARCHAR(255)   NOT NULL,
   PRIMARY KEY (productID)
 );
-
-/*CREATE TABLE orders (
-  orderID        INT(11)        NOT NULL   AUTO_INCREMENT,
-  customerID     INT            NOT NULL,
-  orderDate      DATETIME       NOT NULL,
-  PRIMARY KEY (orderID)
-); 
-
--- insert data into the database
-INSERT INTO categories VALUES
-(1, 'G'),
-(2, 'B'),
-(3, 'D'); */
 
 INSERT INTO products VALUES
 (1, 'Milk'),
